e130274a6ad9705d9cb5c3dc5a3c65fc0e9fddcf
MARANHAOBELLA <isabele.maranhao@etec.sp.gov.br>
2024-04-24 14:41:06 -0300

    formulario php

---
+++ calcular.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    
+        <?php
+       
+        ?>
+
+        <h2>Formulário em PHP</h2>
+    <p><span class="erro">* obrigatório</span></p>
+
+    <form method="post" action="<?php ?>">
+
+    Name: <input type="text" name="name">
+
+    </form>
+
+</body>
+</html>
